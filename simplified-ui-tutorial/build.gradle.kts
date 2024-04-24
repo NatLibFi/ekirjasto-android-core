560183863cff4240782ba5961ced069a6d65ad0d
Mikko <mikko@mitale.fi>
2024-04-24 15:46:52 +0300

    Added credentials lib references

--- simplified-ui-tutorial/build.gradle.kts
+++ simplified-ui-tutorial/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.runtime)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.playauth)
     implementation(libs.androidx.cursoradapter)
     implementation(libs.androidx.customview)
     implementation(libs.androidx.customview.poolingcontainer)
