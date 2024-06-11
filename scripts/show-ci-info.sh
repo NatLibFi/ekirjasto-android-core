f6bc911a6bb36e2cb93d3f37c44012c927e5459d
Sami Kankaristo <sami@kankaristo.fi>
2024-06-11 18:49:47 +0300

    Check if secrets are available in CI run
    If a non-member (e.g. Dependabot) creates a PR, secrets will not be
    available in CI builds. This is good for security, but can be confusing,
    so this PR adds a check for that, and shows a warning+explanation if
    secrets are not available.

--- scripts/show-ci-info.sh
+++ scripts/show-ci-info.sh
@@ -3,7 +3,7 @@
 #
 # Show CI info.
 #
-# 1.2.0
+# 1.3.0
 #
 
 trap 'trap - INT; exit $((128 + $(kill -l INT)))' INT
@@ -18,3 +18,10 @@ echo "COMMIT_SHA=$COMMIT_SHA"
 echo "BRANCH_NAME=$BRANCH_NAME"
 echo "TARGET_BRANCH_NAME=$TARGET_BRANCH_NAME"
 echo "----------------------------------------"
+echo
+echo "Checking if secrets are available, fake secret: $TEST_SECRET"
+if [ -z "$TEST_SECRET" ]; then
+    echo "::warning title=Secrets are unavailable::Secrets are not available, this could be a non-member PR (there could be build issues)"
+else
+    echo "Secrets are available!"
+fi
