e9e85d154b5063813240b264ae7e65a08600397c
Sade-Tuuli Siipola <sade-tuuli.siipola@helsinki.fi>
2024-08-08 16:40:11 +0300

    Delete unused code

--- simplified-profiles-controller-api/src/main/java/org/nypl/simplified/profiles/controller/api/ProfileDependentsLookupRequest.kt
+++
@@ -1,8 +0,0 @@
-package org.nypl.simplified.profiles.controller.api
-
-sealed class ProfileDependentsLookupRequest {
-  data class Ekirjasto(
-    val patronInfo: String,
-    val ekirjastoToken: String,
-  ) : ProfileDependentsLookupRequest()
-}
